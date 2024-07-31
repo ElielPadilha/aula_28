5ab6386fc57f4289bdccdce77ef10e97d8018149
ElielPadilha <irmaoeliel@hotmail.com>
2024-07-30 22:13:46 -0300

    aula 28

---
+++ curriulum eliel/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/inicio")
+def ola():
+    lista = ["O Senhor dos Anéis", "Dom Casmurro", "O Alquimista"]
+    return render_template(
+        "lista.html", titulo="Listagem de Livros - SENAI", lista_de_livros=lista
+    )
+
+
+@app.route("/curriculo")
+def curriculo():
+    return render_template("curriculo.html")
+
+
+app.run(debug=True)
